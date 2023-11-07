423f46f03bc392cef1a74afd70ca7eaea3281b84
YoonJunHyeon <qmdldos@gmail.com>
2023-11-07 10:28:57 +0900

    -에러구문 처리

--- dev13-1.js
+++ dev13-1.js
@@ -1,5 +1,5 @@
 
-const isTexFile = (fileName) => {
+const isTextFile = (fileName) => {
   // 경로의 확장자를 변환
   let result = path.extname(fileName).toLowerCase() === ".txt";
   return result // 문자열로 반환
@@ -7,9 +7,13 @@ const isTexFile = (fileName) => {
 
 const compareFiles = (operator, operand) => {
   try{
-
+    // 파일 확장자 검사
+    if(!isTextFile(operator)|| !isTextFile(operand)){
+      console.error('폴더가 .tst 파일이 아닙니다.');
+      return false;
+    }
   }catch(err){
     console.error('에러 리딩 파일 : ', err);
-    return false
+    return false;
   }
 }
